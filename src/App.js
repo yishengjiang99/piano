33ff94ed8a2f351210b58f0098127b280324cb76
yisheng.jiang <yisheng.jiang@walmart.com>
2020-06-30 23:42:53 -0700

    moved files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,85 @@
+import Sequence from "./sequence";
+import { useState, useRef, useEffect } from "react";
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
-function App() {
+import EnvelopConfig from "./envelop-config";
+import Piano from "./piano";
+import Timer from "./timer";
+
+export const IndexPage = (props) => {
+  const [settings, setSettings] = useState({
+    envelope: {
+      attack: 0.1,
+      decay: 0.1,
+      sustain: 0.1,
+      release: 0.1, //0.01
+    },
+    volume: {
+      min: 0,
+      max: 6,
+    },
+  });
+  const [userEvent, setUserEvent] = useState(null);
+  const [playPosition, setPlayPosition] = useState({
+    tick: 0,
+    systemTime: null,
+  });
+  const [track, setTrack] = useState({});
+  const [total, setTotal] = useState(0);
+  function updateAttribute(attribute, value) {
+    setSettings({
+      ...settings,
+      attribute: value,
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
-  );
-}
+    <>
+      <details>
+        <summary>AM ADSR</summary>
+        <EnvelopConfig
+          style={{ maxWidth: "200px" }}
+          defaults={settings.envelope}
+          onInput={updateAttribute}
+        ></EnvelopConfig>
+      </details>
+      <Timer
+        playPosition={playPosition}
+        total={total}
+        tracks={track}
+        gOnTick={({ tick, time }) => {
+          setPlayPosition({
+            tick: tick,
+            systemTime: time,
+          });
+        }}
+      ></Timer>
+      <Sequence
+        track={track}
+        newEvent={userEvent}
+        onNewNote={function (newNote) {
+          window.redux().addNoteToTrack(newNote);
+        }}
+        rows={12}
+        cols={20}
+      />
+      <Piano
+        track={track}
+        onUserEvent={(type, freq, time, index) => {
+          setUserEvent({
+            time: time,
+            type: type,
+            freq: freq,
+            index: index,
+          });
+        }}
+        octave={3}
+      ></Piano>
 
-export default App;
+      <details>
+        <summary>Console</summary>
+        <div id="console"></div>
+      </details>
+    </>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import { IndexPage } from "./pages/index";
+import { IndexPage } from "./App";
 import * as serviceWorker from "./serviceWorker";
 ReactDOM.render(
   <React.StrictMode>
@@ -14,3 +13,27 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register();
+
+window.redux = function () {
+  window.reduxStorage = window.reduxStorage || {
+    track: {},
+    trackLength: 0,
+    settings: {},
+  };
+  let store = window.reduxStorage;
+  return {
+    store: window.reduxStorage,
+    addNoteToTrack: (note) => {
+      const _track = window.reduxStorage.track;
+      if (!_track[note.bar]) _track[note.bar] = {};
+      _track[note.bar][note.index] = note;
+      store.track = _track; // { track: _track });
+      if (note.bar > store.trackLength) store.trackLength = note.bar;
+    },
+    getStoragedValue: (key) => {
+      return window.reduxStorage[key];
+    },
+    getTrack: () => [store.track, store.trackLength],
+    mapDispatchToProps: () => alert("fuck off"),
+  };
+};
