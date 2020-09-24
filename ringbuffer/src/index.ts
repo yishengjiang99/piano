4bdef5c1c65af34fa68e461d6cbbf8a61018ffb3
Yisheng Jiang <yisheng.jiang@gmail.com>
2020-09-24 02:45:06 -0700

    s

--- ringbuffer/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./csv-transform";
-export * from "./read-line-transform";
-export * from "./convert-midi";
-export * from "./sound-font-loader";
--- src/audio_sources.js
+++ src/audio_sources.js
@@ -1,11 +1,11 @@
 
-const { MembraneSynth, MetalSynth, MetalSynthOptions, MembraneSynthOptions, Frequency, NoiseSynth } = require("tone")
+const { MembraneSynth,MetalSynth,MetalSynthOptions,MembraneSynthOptions,Frequency,NoiseSynth } = require("tone")
 const { Envelope } = require("./envelope");
-const { noteToMajorTriad, frequencyToMidi } = require("./sound-keys");
+const { noteToMajorTriad,frequencyToMidi } = require("./sound-keys");
 var Soundfont = require('soundfont-player');
-module.exports = async function (ctx, inputNode, _settings) {
+export const initAudioSources = async (ctx,inputNode,_settings) => {
 
-    const sfinnst = await Soundfont.instrument(ctx, "https://gleitz.github.io/midi-js-soundfonts/FluidR3_GM/marimba-mp3.js")
+    const sfinnst = await Soundfont.instrument(ctx,"https://gleitz.github.io/midi-js-soundfonts/FluidR3_GM/marimba-mp3.js")
 
     const noteCache = {};
     return {
@@ -20,21 +20,21 @@ module.exports = async function (ctx, inputNode, _settings) {
             }
         },
         getLPSaw: function (freq) {
-            const osc = new OscillatorNode(ctx, { type: 'sawtooth', frequency: freq });
-            const lpf = new BiquadFilterNode(ctx, { type: "lowpass", frequency: 2 * freq });
-            const gain = new GainNode(ctx, { gain: 0 });
+            const osc = new OscillatorNode(ctx,{ type: 'sawtooth',frequency: freq });
+            const lpf = new BiquadFilterNode(ctx,{ type: "lowpass",frequency: 2 * freq });
+            const gain = new GainNode(ctx,{ gain: 0 });
             osc.connect(lpf).connect(gain).connect(inputNode);
             osc.start();
-            const env = Envelope(_settings.adsr, ctx, gain.gain);
+            const env = Envelope(_settings.adsr,ctx,gain.gain);
             return env;
         },
         getDrums: function (freq) {
             const stdEnvelope = (toneSynth) => {
-                var start, release;
+                var start,release;
                 return {
                     trigger: function () {
                         start = ctx.currentTime;
-                        toneSynth.triggerAttack(start, 0.2);
+                        toneSynth.triggerAttack(start,0.2);
 
                     },
                     triggerRelease: function () {
@@ -62,7 +62,7 @@ module.exports = async function (ctx, inputNode, _settings) {
 
             noise.toDestination();
 
-            return stdEnvelope(noise, Frequency(freq).toNote());
+            return stdEnvelope(noise,Frequency(freq).toNote());
 
 
         },
@@ -71,26 +71,26 @@ module.exports = async function (ctx, inputNode, _settings) {
             if (noteCache[hashkey] && ctx.currentTime - noteCache[hashkey].started > 5)
                 return noteCache[hashkey];
 
-            const outputGain = new GainNode(ctx, { gain: 0 });
+            const outputGain = new GainNode(ctx,{ gain: 0 });
             var chords = noteToMajorTriad(freq); // _settings.overtone.map((multiplier) => freqs * multiplier);
             chords
-                .map((freq, idx) => {
-                    return new OscillatorNode(ctx, {
+                .map((freq,idx) => {
+                    return new OscillatorNode(ctx,{
                         type: _settings.osc3[idx],
                         frequency: freq,
                         detune: _settings.detune[idx],
                     });
                 })
-                .map((osc, idx) => {
+                .map((osc,idx) => {
                     idx = idx % 3;
-                    var _gain = new GainNode(ctx, { gain: _settings.overtone[idx] });
-                    var delay = new DelayNode(ctx, { delay: _settings.delay[idx] });
+                    var _gain = new GainNode(ctx,{ gain: _settings.overtone[idx] });
+                    var delay = new DelayNode(ctx,{ delay: _settings.delay[idx] });
                     osc.connect(delay).connect(_gain); //new GainNode(ctx, { gain: _settings.gains[idx] }))
                     _gain.connect(outputGain);
                     osc.start(0);
                 });
             outputGain.connect(inputNode);
-            var gainEnvelope = new Envelope(_settings.adsr, ctx, outputGain.gain);
+            var gainEnvelope = new Envelope(_settings.adsr,ctx,outputGain.gain);
             return gainEnvelope;
         }
     }
