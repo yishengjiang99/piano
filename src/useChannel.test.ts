44219ea9aaed2c89dd91690aab1e708d5eda624e
Yisheng Jiang <yisheng.jiang@gmail.com>
2020-11-18 07:48:06 -0800

    save prog

---
+++ src/useChannel.test.ts
@@ -0,0 +1,7 @@
+// import { renderHook, act } from "@testing-library/react-hooks";
+// import { useChannel } from "./useChannel";
+
+// test("useChannel init", () => {
+//   const { result } = renderHook(() => useChannel<string>("test"));
+//   expect(result.current.length).toEqual(2); // .latestMessage).toBeDefined;
+// });
